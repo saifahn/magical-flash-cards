4c75324a5ec504947f6972568bff3bc6b0bb24a6
Sean Li <li.saifahn@gmail.com>
2019-10-04 13:25:35 +0100

    [8] Make sparse cards consistent width
    Previously, cards with little text did not span the full width of their
    column in text mode. This has been remedied.

--- src/main/cards/Card/Card.js
+++ src/main/cards/Card/Card.js
@@ -14,6 +14,7 @@ const BaseCard = styled.div`
   margin-bottom: 0.5rem;
   border-left: 4px solid ${props => props.theme.primary || '#5a114d'};
   color: #333;
+  width: 100%;
 
   ${media.sm`
     border-left: 5px solid #5a114d;
